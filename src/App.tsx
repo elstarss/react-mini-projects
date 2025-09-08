b96f86f620653fff26f3de0c24b198aab1617317
Ella B <ella55ball@gmail.com>
2025-09-08 14:02:44 +0100

    adding react-router and modularising pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
-
-import "./App.scss";
+import { BrowserRouter, Routes, Route, Link } from "react-router";
+import Home from "./home";
 
 function App() {
     return (
-        <>
-            <h1>hello world</h1>
-        </>
+        <BrowserRouter>
+            <nav>
+                <Link to="/">Home</Link> | <Link to="/about">Project 1</Link> |{" "}
+                <Link to="/contact">Project 2</Link>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                {/* <Route path="/project-1" element={<P1 />} />
+                <Route path="/project-2" element={<P2 />} /> */}
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ src/home/index.tsx
@@ -0,0 +1,10 @@
+import "./index.scss";
+
+export default function Home() {
+    return (
+        <div className="home-div">
+            <h1>Home page</h1>
+            <p>Here is the home</p>
+        </div>
+    );
+}
